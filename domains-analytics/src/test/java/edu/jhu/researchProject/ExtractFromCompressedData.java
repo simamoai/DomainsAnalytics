6e744a2b67eb44887b1311be012b8e36f950dc89
Tuan Nguyen <tuannguyen18@gmail.com>
2013-08-09 14:47:39 -0400

    Parsed domain names with other fields, print out.

--- domains-analytics/src/test/java/edu/jhu/researchProject/ExtractFromCompressedData.java
+++ domains-analytics/src/test/java/edu/jhu/researchProject/ExtractFromCompressedData.java
@@ -1,9 +1,9 @@
 package edu.jhu.researchProject;
 
 import java.io.IOException;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.fs.Path;
@@ -26,42 +26,57 @@ public class ExtractFromCompressedData {
 		FileOutputFormat.setOutputPath(job, new Path(
 				"output/extractCompressedData.out"));
 
-		job.setMapperClass(WordMapper.class);
-		job.setReducerClass(SumReducer.class);
+		job.setMapperClass(DomainNamesMapper.class);
+		job.setReducerClass(DomainNamesReducer.class);
 
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
 
 		boolean success = job.waitForCompletion(true);
 		System.exit(success ? 0 : 1);
 	}
 
-	public static class WordMapper extends
-			Mapper<LongWritable, Text, Text, IntWritable> {
+	public static enum MyCounter {
+	    TOTAL_RECORDS_PROCESSED_IN_MAP
+	}
+
+	public static class DomainNamesMapper extends
+			Mapper<LongWritable, Text, Text, Text> {
 		@Override
 		public void map(LongWritable key, Text value, Context context)
 				throws IOException, InterruptedException {
 			String line = value.toString();
-
-			for (String word : line.split("\\W+")) {
-				if (word.length() > 0) {
-					context.write(new Text(word), new IntWritable(1));
-				}
+			String[] arr = line.split(" ");
+			Text domainKey = new Text();
+			Text values = new Text();
+			
+			if (arr.length == 3) {
+				domainKey.set(arr[0]);
+				values.set(arr[1]+"~"+arr[2]);
+				context.write(domainKey, values);
 			}
+			Counter totalRecordsProcessedCounter = context.getCounter(MyCounter.TOTAL_RECORDS_PROCESSED_IN_MAP);
+	        totalRecordsProcessedCounter.increment(1);
 		}
 	}
 
-	public static class SumReducer extends
-			Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class DomainNamesReducer extends
+			Reducer<Text, Text, Text, Text> {
 		@Override
-		public void reduce(Text key, Iterable<IntWritable> values,
+		public void reduce(Text key, Iterable<Text> values,
 				Context context) throws IOException, InterruptedException {
-			int wordCount = 0;
-
-			for (IntWritable value : values) {
-				wordCount += value.get();
+			String number = "";
+			String ipAddress = "";
+			
+			for (Text value :  values) {
+				String[] valuesArr = value.toString().split("~");
+				if (valuesArr.length == 2) {
+					number = valuesArr[0];
+					ipAddress = valuesArr[1];
+				}
 			}
-			context.write(key, new IntWritable(wordCount));
+			String outputString = number+" "+ipAddress;
+			context.write(key, new Text(outputString));
 		}
 	}
 }
