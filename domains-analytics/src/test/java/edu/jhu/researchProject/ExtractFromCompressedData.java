c5add45038374c16eaa58979a9bc45b8b02819cd
Tuan Nguyen <tuannguyen18@gmail.com>
2013-08-09 02:22:38 -0400

    Add test data and initial word count map reduce job to test project setup.

--- domains-analytics/src/test/java/edu/jhu/researchProject/ExtractFromCompressedData.java
+++ domains-analytics/src/test/java/edu/jhu/researchProject/ExtractFromCompressedData.java
@@ -12,51 +12,56 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 
 public class ExtractFromCompressedData {
-	
-	public void run() throws IOException, InterruptedException, ClassNotFoundException {
-	    Job job = new Job();
-	    
-	    job.setJarByClass(ExtractFromCompressedData.class);
-	    
-	    job.setJobName("Extract From Compressed Data");
 
-	    FileInputFormat.setInputPaths(job, new Path("/home/training/Downloads/com.zone1.gz"));
-	    FileOutputFormat.setOutputPath(job, new Path("extractCompressedData2.out"));
+	public void run() throws IOException, InterruptedException,
+			ClassNotFoundException {
+		Job job = new Job();
 
-	    job.setMapperClass(WordMapper.class);
-	    job.setReducerClass(SumReducer.class);
+		job.setJarByClass(ExtractFromCompressedData.class);
 
-	    job.setOutputKeyClass(Text.class);
-	    job.setOutputValueClass(IntWritable.class);
+		job.setJobName("Extract From Compressed Data");
 
-	    boolean success = job.waitForCompletion(true);
-	    System.exit(success ? 0 : 1);
-	  }
+		FileInputFormat.setInputPaths(job, new Path(this.getClass()
+				.getClassLoader().getResource("com.zone1.gz").toString()));
+		FileOutputFormat.setOutputPath(job, new Path(
+				"output/extractCompressedData.out"));
 
-	public static class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-	  @Override
-	  public void map(LongWritable key, Text value, Context context)
-	      throws IOException, InterruptedException {
-	    String line = value.toString();
+		job.setMapperClass(WordMapper.class);
+		job.setReducerClass(SumReducer.class);
 
-	    for (String word : line.split("\\W+")) {
-	      if (word.length() > 0) {
-	        context.write(new Text(word), new IntWritable(1));
-	      }
-	    }
-	  }
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		boolean success = job.waitForCompletion(true);
+		System.exit(success ? 0 : 1);
 	}
-	
-	public static class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-		  @Override
-			public void reduce(Text key, Iterable<IntWritable> values, Context context)
-					throws IOException, InterruptedException {
-				int wordCount = 0;
-				
-				for (IntWritable value : values) {
-					wordCount += value.get();
+
+	public static class WordMapper extends
+			Mapper<LongWritable, Text, Text, IntWritable> {
+		@Override
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String line = value.toString();
+
+			for (String word : line.split("\\W+")) {
+				if (word.length() > 0) {
+					context.write(new Text(word), new IntWritable(1));
 				}
-				context.write(key, new IntWritable(wordCount));
 			}
 		}
+	}
+
+	public static class SumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int wordCount = 0;
+
+			for (IntWritable value : values) {
+				wordCount += value.get();
+			}
+			context.write(key, new IntWritable(wordCount));
+		}
+	}
 }
