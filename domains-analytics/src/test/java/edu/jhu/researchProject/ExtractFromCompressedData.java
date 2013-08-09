bf571200d8ecccbc3304ac561868fbb86c84c3c7
Tuan Nguyen <tuannguyen18@gmail.com>
2013-08-09 02:08:20 -0400

    First stage of Extract from Compressed Data test only..

---
+++ domains-analytics/src/test/java/edu/jhu/researchProject/ExtractFromCompressedData.java
@@ -0,0 +1,62 @@
+package edu.jhu.researchProject;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+
+public class ExtractFromCompressedData {
+	
+	public void run() throws IOException, InterruptedException, ClassNotFoundException {
+	    Job job = new Job();
+	    
+	    job.setJarByClass(ExtractFromCompressedData.class);
+	    
+	    job.setJobName("Extract From Compressed Data");
+
+	    FileInputFormat.setInputPaths(job, new Path("/home/training/Downloads/com.zone1.gz"));
+	    FileOutputFormat.setOutputPath(job, new Path("extractCompressedData2.out"));
+
+	    job.setMapperClass(WordMapper.class);
+	    job.setReducerClass(SumReducer.class);
+
+	    job.setOutputKeyClass(Text.class);
+	    job.setOutputValueClass(IntWritable.class);
+
+	    boolean success = job.waitForCompletion(true);
+	    System.exit(success ? 0 : 1);
+	  }
+
+	public static class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	  @Override
+	  public void map(LongWritable key, Text value, Context context)
+	      throws IOException, InterruptedException {
+	    String line = value.toString();
+
+	    for (String word : line.split("\\W+")) {
+	      if (word.length() > 0) {
+	        context.write(new Text(word), new IntWritable(1));
+	      }
+	    }
+	  }
+	}
+	
+	public static class SumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		  @Override
+			public void reduce(Text key, Iterable<IntWritable> values, Context context)
+					throws IOException, InterruptedException {
+				int wordCount = 0;
+				
+				for (IntWritable value : values) {
+					wordCount += value.get();
+				}
+				context.write(key, new IntWritable(wordCount));
+			}
+		}
+}
---
+++ domains-analytics/src/test/java/edu/jhu/researchProject/ExtractFromCompressedDataTest.java
@@ -0,0 +1,10 @@
+package edu.jhu.researchProject;
+
+import org.junit.Test;
+
+public class ExtractFromCompressedDataTest {
+	@Test
+	public void testExtractCompressedData() throws Exception{
+		new ExtractFromCompressedData().run();
+	}
+}
